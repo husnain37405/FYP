a50018eaf9bbb3a9b7883fbf2ab016370e091c14
Muhammad Husnain Jahangir <husnain.jahangir2002@gmail.com>
2025-01-27 11:23:29 +0500

    changed name of folder

--- frontend/src/pages/AppRoutes.jsx
+++ frontend/src/pages/AppRoutes.jsx
@@ -13,27 +13,27 @@ import RequesterLogin from './RequesterLogin';
 import DonorSignup from './DonorSignup';
 import RequestSignup from './RequestSignup';
 import DashboardCards from '../components/Dashboard/DashboardCards';
-import Users from '../components/Pages/Users';
-import Projects from '../components/Pages/Projects';
-import AdminDonations from '../components/Pages/AdminDonations';
-import AdminRequests from '../components/Pages/AdminRequests';
+import Users from '../components/Admin/Users';
+import Projects from '../components/Admin/Projects';
+import AdminDonations from '../components/Admin/AdminDonations';
+import AdminRequests from '../components/Admin/AdminRequests';
 import AddDonation from '../components/UserPages/AddDonation';
 import DonationHistory from '../components/UserPages/DonationHistory';
-import Profile from '../components/Pages/Profile';
+import Profile from '../components/Admin/Profile';
 import UserProjects from '../components/UserPages/UserProjects';
 import UserProfile from '../components/UserPages/UserProfile';
-import AdminReports from '../components/Pages/AdminReports';
-import CurrentMonth from '../components/Pages/CurrentMonth';
+import AdminReports from '../components/Admin/AdminReports';
+import CurrentMonth from '../components/Admin/CurrentMonth';
 import NotFound from './NotFound';
 import DonorDashboardCards from '../components/UserDashboard/DonorDashboardCards';
 import RequesterDashboardCards from '../components/UserDashboard/RequesterDashboardCards';
-import DonorTable from '../components/Pages/Tables/DonorTable';
-import RequesterTable from '../components/Pages/Tables/RequesterTable'
-import AddProject from '../components/Pages/AddProject';
+import DonorTable from '../components/Admin/Tables/DonorTable';
+import RequesterTable from '../components/Admin/Tables/RequesterTable'
+import AddProject from '../components/Admin/AddProject';
 import RequestHistory from '../components/UserPages/RequestHistory';
 import AddRequest from '../components/UserPages/AddRequest';
 import EditProfile from '../components/UserPages/EditProfile/EditProfile';
-import AdminProfileEdit from '../components/Pages/AdminProfileEdit/AdminProfileEdit'
+import AdminProfileEdit from '../components/Admin/AdminProfileEdit/AdminProfileEdit'
 
 const AppRoutes = ({ toggleTheme, theme }) => {
   return (
